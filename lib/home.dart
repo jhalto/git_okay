296c442a4e23bf71883ae29cdb5f53b2fea9cf4a
Zobayer Arman Nadim <zobayerarmannadim@gmail.com>
2025-01-14 18:17:12 +0600

    added some

---
+++ lib/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      dsfadshf  
+      iuhdsafiouy bottomNavigationBar: uiasdfi 
+      udsiyafi udsiyafihy Scaffold],oijs
+      hdsfkjhlsadk
+      jdhsfkjh
+      jsdhfkjhasd
+      dsjhfkjhasd
+      kdsjahfkj 
+      jsdhf 
+      
+      dsfkljsda
+        );
+  }
+}
